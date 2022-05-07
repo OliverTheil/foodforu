8ef6fb9cf2f796b00a5ccfae2fab1f66e2d96ac4
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-05-07 15:35:03 +0200

    enable angular and start of project

---
+++ project/foodforu/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { BodyComponent } from './body/body.component';
+import { ItemComponent } from './item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    BodyComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
